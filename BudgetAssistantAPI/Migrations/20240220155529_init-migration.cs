b7caf2e409a13a93478f56839c0a88ebb5c1a269
Mykola <mykolaivanov0201@gmail.com>
2024-02-20 21:20:44 +0200

    Cereated Migration. Added configuration for database context in "Program.cs" file. Added Place Controller.

---
+++ BudgetAssistantAPI/Migrations/20240220155529_init-migration.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetAssistantAPI.Migrations
+{
+    public partial class initmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BudgetGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BudgetRecordTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BudgetRecordTypeName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetRecordTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountryName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Districts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DistrictName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CountryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Districts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Districts_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BudgetGroupUser",
+                columns: table => new
+                {
+                    BudgetGroupsId = table.Column<int>(type: "int", nullable: false),
+                    UsersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetGroupUser", x => new { x.BudgetGroupsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_BudgetGroupUser_BudgetGroups_BudgetGroupsId",
+                        column: x => x.BudgetGroupsId,
+                        principalTable: "BudgetGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BudgetGroupUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Settlements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SettlementName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DistrictId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settlements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Settlements_Districts_DistrictId",
+                        column: x => x.DistrictId,
+                        principalTable: "Districts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Streets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StreetName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SettlementId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Streets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Streets_Settlements_SettlementId",
+                        column: x => x.SettlementId,
+                        principalTable: "Settlements",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Buildings",
+                columns: table => new
+                {
+                    BuildingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BuildingCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StreetId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buildings", x => x.BuildingId);
+                    table.ForeignKey(
+                        name: "FK_Buildings_Streets_StreetId",
+                        column: x => x.StreetId,
+                        principalTable: "Streets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Places",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlaceName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BuildingId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Places", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Places_Buildings_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Buildings",
+                        principalColumn: "BuildingId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BudgetRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Currency = table.Column<double>(type: "float", nullable: false),
+                    AddedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AddedById = table.Column<int>(type: "int", nullable: true),
+                    PlaceId = table.Column<int>(type: "int", nullable: true),
+                    BudgetRecordTypeId = table.Column<int>(type: "int", nullable: true),
+                    BudgetGroupId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BudgetRecords_BudgetGroups_BudgetGroupId",
+                        column: x => x.BudgetGroupId,
+                        principalTable: "BudgetGroups",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BudgetRecords_BudgetRecordTypes_BudgetRecordTypeId",
+                        column: x => x.BudgetRecordTypeId,
+                        principalTable: "BudgetRecordTypes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BudgetRecords_Places_PlaceId",
+                        column: x => x.PlaceId,
+                        principalTable: "Places",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BudgetRecords_Users_AddedById",
+                        column: x => x.AddedById,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetGroupUser_UsersId",
+                table: "BudgetGroupUser",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetRecords_AddedById",
+                table: "BudgetRecords",
+                column: "AddedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetRecords_BudgetGroupId",
+                table: "BudgetRecords",
+                column: "BudgetGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetRecords_BudgetRecordTypeId",
+                table: "BudgetRecords",
+                column: "BudgetRecordTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetRecords_PlaceId",
+                table: "BudgetRecords",
+                column: "PlaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buildings_StreetId",
+                table: "Buildings",
+                column: "StreetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Districts_CountryId",
+                table: "Districts",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Places_BuildingId",
+                table: "Places",
+                column: "BuildingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Settlements_DistrictId",
+                table: "Settlements",
+                column: "DistrictId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Streets_SettlementId",
+                table: "Streets",
+                column: "SettlementId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BudgetGroupUser");
+
+            migrationBuilder.DropTable(
+                name: "BudgetRecords");
+
+            migrationBuilder.DropTable(
+                name: "BudgetGroups");
+
+            migrationBuilder.DropTable(
+                name: "BudgetRecordTypes");
+
+            migrationBuilder.DropTable(
+                name: "Places");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Buildings");
+
+            migrationBuilder.DropTable(
+                name: "Streets");
+
+            migrationBuilder.DropTable(
+                name: "Settlements");
+
+            migrationBuilder.DropTable(
+                name: "Districts");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
